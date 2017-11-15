79757fbb3f5be79997d0d2ec5d8b450546f0b364
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-11-15 07:05:48 -0500

    Fix draw size
    x and y offsets were backwards

--- SpriteAnimator/Database/DrawSize.java
+++ SpriteAnimator/Database/DrawSize.java
@@ -3,13 +3,13 @@ package SpriteAnimator.Database;
 public enum DrawSize {
 	FULL (0, 0, 16, 16), // old token: F
 	TOP_HALF (0, 0, 16, 8), // old token: T
-	BOTTOM_HALF (8, 0, 16, 8), // old token: B
-	RIGHT_HALF (0, 8, 8, 16), // old token: R
+	BOTTOM_HALF (0, 8, 16, 8), // old token: B
+	RIGHT_HALF (8, 0, 8, 16), // old token: R
 	LEFT_HALF (0, 0, 8, 16), // old token: L
-	TOP_RIGHT (0, 8, 8, 8), // old token: TR
+	TOP_RIGHT (8, 0, 8, 8), // old token: TR
 	TOP_LEFT (0, 0, 8, 8), // old token: TL
 	BOTTOM_RIGHT (8, 8, 8, 8), // old token: BR
-	BOTTOM_LEFT (8, 0, 8, 8), // old token: BL
+	BOTTOM_LEFT (0, 8, 8, 8), // old token: BL
 	TALL_8X24 (0, 0, 8, 24), // old token: XT
 	WIDE_24X8 (0, 0, 24, 8), // old token: XW
 	LARGE_16X24 (0, 0, 16, 24), // old token: XL
